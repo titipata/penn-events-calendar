2d3cf9f628aeddc270bc6b26537594713c3f65cb
bluenex <tulakan.r@gmail.com>
2018-07-04 00:05:04 +0700

    separated item to sub components

---
+++ frontend/src/Components/EventsContainer/EventItem/DetailBoxComponent.js
@@ -0,0 +1,47 @@
+import { FontAwesomeIcon as Fa } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Category } from '../../../Data';
+import CategoryLabel from './CategoryLabelComponent';
+import renderHTML from 'react-render-html';
+import React from 'react';
+
+const DetailWrapper = styled.div`
+  padding: 0 15px;
+`;
+
+const StyledTitle = styled.div`
+  font-size: 1.3rem;
+  font-weight: 600;
+`;
+
+const StyledLocation = styled.div`
+  font-size: 0.9rem;
+  padding-top: 5px;
+`;
+
+const CatContainer = styled.div`
+  display: inline-block;
+`;
+
+const DetailBox = ({ eventTitle, eventLocation, eventCategory }) => (
+  <DetailWrapper>
+    <StyledTitle>
+      {renderHTML(eventTitle)}
+    </StyledTitle>
+    <StyledLocation>
+      <Fa icon="map-marker-alt" /> {eventLocation}
+    </StyledLocation>
+    <CatContainer>
+      <CategoryLabel eventCategory={eventCategory} />
+    </CatContainer>
+  </DetailWrapper>
+);
+
+DetailBox.propTypes = {
+  eventTitle: PropTypes.string.isRequired,
+  eventLocation: PropTypes.string.isRequired,
+  eventCategory: PropTypes.string.isRequired,
+};
+
+export default DetailBox;
--- frontend/src/Components/EventsContainer/index.js
+++ frontend/src/Components/EventsContainer/index.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { Key, Events as evUtil } from '../../Utils';
+import { Key } from '../../Utils';
 import EventItem from './EventItem';
 import EventsList from './EventsList';
 
