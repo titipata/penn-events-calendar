81e0ccf7a28b376c730413991ea75fb586bbc059
titipata <my.titipat@gmail.com>
2018-07-11 23:02:40 -0400

    Update events.json, running cron every day

--- backend/cron_fetch_events.py
+++ backend/cron_fetch_events.py
@@ -8,5 +8,6 @@ print('Len of crontab =', len(cron))
 
 command = sys.executable + ' ' + os.path.join(os.getcwd(), 'fetch_events.py')
 job = cron.new(command=command)
-job.minute.every(1)
+# job.minute.every(1) # for testing
+job.day.every(1)
 cron.write()
