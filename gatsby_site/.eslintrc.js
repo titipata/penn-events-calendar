f7d61e99bfabcb7dc6370117b14ea8139212a8f1
Tulakan Ruangrong <tulakan.r@gmail.com>
2019-09-26 23:03:22 +0700

    Setup test (#65)
    * setting up test runner, change eslint config to js file
    
    * add 2 tests for utils

--- gatsby_site/.eslintrc.js
+++ gatsby_site/.eslintrc.js
@@ -1,22 +1,27 @@
-{
-  "extends": "airbnb",
-  "plugins": [
-    "react",
-    "react-hooks"
+module.exports = {
+  extends: [
+    "airbnb",
+    "react-app",
   ],
-  "parser": "babel-eslint",
-  "rules": {
+  plugins: [
+    "react",
+    "react-hooks",
+    "jest",
+  ],
+  parser: "babel-eslint",
+  rules: {
     "no-nested-ternary": "off",
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "no-underscore-dangle": 0,
     "react/prop-types": ["error", { "ignore": ["navigation", "data"] }],
     "global-require": 0,
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
   },
-  "globals": {
-    "fetch": false,
-    "localStorage": false,
-    "window": false
-  }
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  env: {
+    "jest/globals": true,
+  },
 }
