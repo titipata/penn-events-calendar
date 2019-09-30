df754030d40478c4c0a8dfa4b755b1942ba90deb
tulakann <tulakan.r@gmail.com>
2019-09-30 20:57:46 +0700

    fix svg recieved null fillPercent

--- gatsby_site/src/components/BaseComponents/SvgVerticalGradient.js
+++ gatsby_site/src/components/BaseComponents/SvgVerticalGradient.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SvgVerticalGradient = ({ bgColor, fillColor, fillPercent }) => (
-  <svg width="0" height="0">
-    <linearGradient id={`lgrad-${fillPercent}`} x1={`${fillPercent}%`} y1="100%" x2={`${fillPercent}%`} y2="0%">
-      <stop offset={`${fillPercent}%`} stopColor={fillColor} stopOpacity="1" />
-      <stop offset={`${fillPercent}%`} stopColor={bgColor} stopOpacity="1" />
-    </linearGradient>
-  </svg>
-);
+const SvgVerticalGradient = ({ bgColor, fillColor, fillPercent }) => {
+  if (!fillPercent) {
+    return null;
+  }
+
+  return (
+    <svg width="0" height="0">
+      <linearGradient id={`lgrad-${fillPercent}`} x1={`${fillPercent}%`} y1="100%" x2={`${fillPercent}%`} y2="0%">
+        <stop offset={`${fillPercent}%`} stopColor={fillColor} stopOpacity="1" />
+        <stop offset={`${fillPercent}%`} stopColor={bgColor} stopOpacity="1" />
+      </linearGradient>
+    </svg>
+  );
+};
 
 SvgVerticalGradient.propTypes = {
   bgColor: PropTypes.string.isRequired,
@@ -16,7 +22,11 @@ SvgVerticalGradient.propTypes = {
   fillPercent: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
-  ]).isRequired,
+  ]),
+};
+
+SvgVerticalGradient.defaultProps = {
+  fillPercent: null,
 };
 
 export default SvgVerticalGradient;
