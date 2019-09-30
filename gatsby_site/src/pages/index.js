097bbe0d6712a1cd69e1c70c761385719e582b43
tulakann <tulakan.r@gmail.com>
2019-09-30 21:59:15 +0700

    extract static resources loading to hooks, remove old about page

--- gatsby_site/src/pages/index.js
+++ gatsby_site/src/pages/index.js
@@ -1,29 +1,17 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faBookmark, faCalendarAlt, faChevronCircleDown,
-  faChevronCircleUp, faClock, faCopy, faExternalLinkAlt,
-  faFileAlt, faMapMarkerAlt, faSchool,
-  faStar, faUniversity, faUserTie,
-} from '@fortawesome/free-solid-svg-icons';
 import { graphql } from 'gatsby';
 import React from 'react';
 import EventsContainer from '../components/EventsContainer';
 import Layout from '../components/layout';
 import useLocalStorage from '../hooks/useLocalStorage';
+import useStaticResources from '../hooks/useStaticResources';
 import { Events as evUtil } from '../utils';
-import 'rc-pagination/assets/index.css';
-
-// add fa font to use
-library.add(
-  faCalendarAlt, faMapMarkerAlt, faClock, faFileAlt,
-  faExternalLinkAlt, faUserTie, faSchool, faUniversity,
-  faBookmark, faChevronCircleDown, faChevronCircleUp,
-  faCopy, faStar,
-);
 
 export default ({ data }) => {
+  // load static resources
+  useStaticResources();
   // use this to retrieve data and rehydrate before globalState is used
   useLocalStorage();
+
   // preprocess events before sending to events list
   const preprocessedEvents = evUtil.getPreprocessedEvents(
     data.allEventsJson.edges,
@@ -32,7 +20,7 @@ export default ({ data }) => {
 
   return (
     <Layout>
-      <h1>Upcoming Events</h1>
+      <h1>Upcoming Events |Search Icon|</h1>
       <EventsContainer
         allEvents={preprocessedEvents}
       />
