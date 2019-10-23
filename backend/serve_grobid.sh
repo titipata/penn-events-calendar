066d83aba6998364e26df33c44c4d4edde6a6e01
tulakann <tulakan.r@gmail.com>
2019-10-23 23:42:09 +0700

    fix grobid serve script

--- backend/serve_grobid.sh
+++ backend/serve_grobid.sh
@@ -5,12 +5,10 @@ declare -r version="0.5.5"
 
 if [ ! -d grobid-${version} ]; then
   wget https://github.com/kermitt2/grobid/archive/${version}.zip
-  unzip ${version}.zip
-  rm ${version}.zip
+  unzip "${version}.zip"
+  rm "${version}.zip"
 fi
 
 # run GROBID
 cd grobid-${version} || exit
 ./gradlew run
-
-
