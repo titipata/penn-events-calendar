5276a6c9d112dd7957f5ae29cc9fb1f5d137b23d
titipata <my.titipat@gmail.com>
2019-11-26 20:58:10 -0500

    Change config back to assignment

--- backend/config.py
+++ backend/config.py
@@ -3,10 +3,12 @@ import os
 # Module containing scrapers for extracting data from webpages
 # Every public function (i.e., those not beginning with _) should return a list
 # of event dictionaries when called with no arguments.
-import local.upenn.scrapers as scrapers
+import local.upenn.scrapers
+scrapers = local.upenn.scrapers
 
 # Module containing a list of locations (for feature generation)
-import local.upenn.locations as locations
+import local.upenn.locations
+locations = local.upenn.locations.locations
 
 # Path to save event data (as json)
 PATH_DATA = os.path.join('data', 'events.json')  # path to save events
